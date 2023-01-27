3f6ed75fbc713ade53e796aeefc546943c2b1621
Sukanya Dutta <49310523+Sukanya3096@users.noreply.github.com>
2023-01-28 00:23:07 +0530

    singly linked list - pop

---
+++ Singly Linked Lists/singlyLinkedList.pop.sol.js
@@ -0,0 +1,46 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if(!this.head) {
+            this.head = this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length += 1;
+        return this;
+    }
+
+    pop() {
+        if(!this.head) {
+            return;
+        } else {
+            let current,temp = this.head;
+            while(current.next) {
+                temp = current;
+                current = current.next;               
+            }
+            this.tail = temp;
+            this.tail.next = null;
+            this. length -= 1;
+            if(this.length == 0) {
+                this.head = this.tail = null;
+            }
+            return current;
+        }
+    }
+}
